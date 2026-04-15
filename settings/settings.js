69ea95e5846ffdb44977dc923961fb7a557c2f9c
gruijter <gruijter@hotmail.com>
2026-04-15 11:00:48 +0200

    rewritten settings page

--- settings/settings.js
+++ settings/settings.js
@@ -1,67 +1,69 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
 
+'use strict';
+
 let Homey;
 
 // tab 2 stuff here
 function displayLogs(lines) {
-	$('#loglines').html(lines);
+  $('#loglines').html(lines);
 }
 
 function updateLogs() {
-	try {
-		displayLogs('');
-		Homey.api('GET', 'getlogs/', null)
-			.then((result) => {
-				let lines = '';
-				result
-					.reverse()
-					.forEach((line) => {
-						const logLine = line
-							.replace(' [MyApp]', '')
-							.replace(' [ManagerDrivers]', '')
-							.replace(/\[Device:(.*?)\]/, '[dev]')
-							.replace(/\[Driver:(.*?)\]/, '[$1]');
-						lines += `${logLine}<br />`;
-					});
-				displayLogs(lines);
-			})
-			.catch((err) => {
-				displayLogs(err);
-			});
-	} catch (e) {
-		displayLogs(e);
-	}
+  try {
+    displayLogs('');
+    Homey.api('GET', 'getlogs/', null)
+      .then((result) => {
+        let lines = '';
+        result
+          .reverse()
+          .forEach((line) => {
+            const logLine = line
+              .replace(' [MyApp]', '')
+              .replace(' [ManagerDrivers]', '')
+              .replace(/\[Device:(.*?)\]/, '[dev]')
+              .replace(/\[Driver:(.*?)\]/, '[$1]');
+            lines += `${logLine}<br />`;
+          });
+        displayLogs(lines);
+      })
+      .catch((err) => {
+        displayLogs(err);
+      });
+  } catch (e) {
+    displayLogs(e);
+  }
 }
 
 function deleteLogs() {
-	Homey.confirm(Homey.__('settings.tab2.deleteWarning'), 'warning', (error, result) => {
-		if (result) {
-			Homey.api('GET', 'deletelogs/', null)
-				.then(() => {
-					Homey.alert(Homey.__('settings.tab2.deleted'), 'info');
-					updateLogs();
-				})
-				.catch((err) => {
-					Homey.alert(err.message, 'error');
-				});
-		}
-	});
+  Homey.confirm(Homey.__('settings.tab2.deleteWarning'), 'warning', (error, result) => {
+    if (result) {
+      Homey.api('GET', 'deletelogs/', null)
+        .then(() => {
+          Homey.alert(Homey.__('settings.tab2.deleted'), 'info');
+          updateLogs();
+        })
+        .catch((err) => {
+          Homey.alert(err.message, 'error');
+        });
+    }
+  });
 }
 
 // generic stuff here
 function showTab(tab) {
-	if (tab === 2) updateLogs();
-	$('.tab').removeClass('tab-active');
-	$('.tab').addClass('tab-inactive');
-	$(`#tabb${tab}`).removeClass('tab-inactive');
-	$(`#tabb${tab}`).addClass('active');
-	$('.panel').hide();
-	$(`#tab${tab}`).show();
+  if (tab === 2) updateLogs();
+  $('.tab').removeClass('tab-active');
+  $('.tab').addClass('tab-inactive');
+  $(`#tabb${tab}`).removeClass('tab-inactive');
+  $(`#tabb${tab}`).addClass('active');
+  $('.panel').hide();
+  $(`#tab${tab}`).show();
 }
 
 function onHomeyReady(homeyReady) {
-	Homey = homeyReady;
-	showTab(1);
-	Homey.ready();
+  Homey = homeyReady;
+  showTab(1);
+  Homey.ready();
 }
